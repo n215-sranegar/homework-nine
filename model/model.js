7efeb80dd989a3e38c872f0f19329f09dd5295cd
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2022-04-05 21:35:54 -0400

    Home page complete with all content

--- model/model.js
+++ model/model.js
@@ -1,6 +1,27 @@
 //this information simulates getting data from a database
 
-var homeContent = `<h1>home</h1>`;
+var homeContent = `
+<div class="hero">
+<h1>Winter Park Resort</h1>
+<p>Winter Park, CO</p></div>
+<div class="booking-container">
+<h2>Book Early and Save!</h2>
+<div class="home-booking-buttons-wrapper">
+<div class="home-booking-buttons">
+<h3>Lodging</h3></div>
+<div class="home-booking-buttons">
+<h3>Lift Ticketes</h3></div>
+<div class="home-booking-buttons">
+<h3>Rentals</h3></div>
+<div class="home-booking-buttons">
+<h3>Tubing Hill</h3></div>
+<div class="home-booking-buttons">
+<h3>Season Passes</h3></div>
+</div>
+</div>
+<div class="lodging">
+<h2>On-site Lodging</h2>
+<div class="button-wrapper"><button>Book Now!</button></div></div>`;
 var aboutContent = `<h1>about</h1>`;
 var productsContent = `<h1>products</h1>`;
 var contactContent = `<h1>contact</h1>`;
